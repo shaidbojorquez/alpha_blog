73d01fb6a3669a760407cdb77e5fec120345f8aa
Shaid <shaid.bojorquez@gmail.com>
2021-07-13 18:13:37 -0500

    Index route, action and view added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
-  resources :articles, only: [:show]#Me da todas las rutas que quiero de los articulos
+  resources :articles, only: [:show, :index]#Me da todas las rutas que quiero de los articulos
 end
