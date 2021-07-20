a5ac50162e2216f4eeda6c647d8f0c68650ff4e6
Shaid <shaid.bojorquez@gmail.com>
2021-07-19 19:26:58 -0500

    Refactorized code

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,9 @@
 class ArticlesController < ApplicationController
-
+    before_action :set_article, only: [:show, :edit, :update, :destroy]
+    
+    
     def show
-        @article = Article.find(params[:id])
+       
     end
 
     def index
@@ -14,12 +16,12 @@ class ArticlesController < ApplicationController
     end
 
     def edit
-        @article = Article.find(params[:id])
+      
     end
 
     def create
         #render plain: params[:article]
-        @article = Article.new(params.require(:article).permit(:title, :description)) 
+        @article = Article.new(article_params) 
         if @article.save
             flash[:notice] = "Article was created successfully"
             redirect_to @article
@@ -29,8 +31,8 @@ class ArticlesController < ApplicationController
     end
 
     def update
-        @article = Article.find(params[:id])
-        if @article.update(params.require(:article).permit(:title, :description))
+       
+        if @article.update(article_params)
             flash[:notice] = "Article was updated successfully"
             redirect_to @article
         else
@@ -39,9 +41,19 @@ class ArticlesController < ApplicationController
     end
 
     def destroy
-        @article = Article.find(params[:id])
+        
         @article.destroy
         redirect_to articles_path
+    end
+
+    private 
+
+    def set_article
+        @article = Article.find(params[:id])
+    end
+
+    def article_params
+        params.require(:article).permit(:title, :description)
     end
 
 end
